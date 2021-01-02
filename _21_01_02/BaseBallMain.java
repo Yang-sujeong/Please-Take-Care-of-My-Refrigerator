71ff6105d062bef5d23fb579ab8f7acdad7b1746
Yang-sujeong <72591569+Yang-sujeong@users.noreply.github.com>
2021-01-02 18:09:00 +0900

    Add files via upload

---
+++ _21_01_02/BaseBallMain.java
@@ -0,0 +1,14 @@
+package til._21_01_02;
+
+import java.util.Scanner;
+
+public class BaseBallMain {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BallGame game = new BallGame();
+		
+		game.mainRun();
+}
+
+}
